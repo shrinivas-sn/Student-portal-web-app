eafd85b7860a5b257025044b22d9814f8ef5cec2
SHRINIVAS NEMAGOUDAR <shrinusn2001@gmail.com>
2026-01-07 14:32:42 +0530

    Update firebase.js

--- public/js/config/firebase.js
+++ public/js/config/firebase.js
@@ -1,6 +1,10 @@
      // Your web app's Firebase configuration
 const firebaseConfig = {
-            
-        };
-        const cloudinaryConfig = {
+    apiKey: "YOUR_API_KEY_HERE",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_ID",
+    appId: "YOUR_APP_ID"
+};
   
